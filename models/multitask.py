bbf8cf7f497c850874a3759140c6c3d32b53be10
SumeetD001 <ee22b170@smail.iitm.ac.in>
2026-04-10 18:20:24 +0530

    Update multitask.py

--- models/multitask.py
+++ models/multitask.py
@@ -47,6 +47,6 @@ class MultiTaskPerceptionModel(nn.Module):
 
         return {
             "classification": cls,
-            "localization": box,
+            "localization": box*224,
             "segmentation": seg
         }
