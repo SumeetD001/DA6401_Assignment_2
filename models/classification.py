33310104d7322639cb0d1e293239702263100440
SumeetD001 <ee22b170@smail.iitm.ac.in>
2026-04-10 13:00:27 +0530

    Update classification.py

--- models/classification.py
+++ models/classification.py
@@ -1,1 +1,16 @@
 
+import torch.nn as nn
+from models.layers import CustomDropout
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(512*7*7,1024),
+            nn.ReLU(),
+            CustomDropout(0.5),
+            nn.Linear(1024,37)
+        )
+
+    def forward(self,x):
+        return self.fc(x)
